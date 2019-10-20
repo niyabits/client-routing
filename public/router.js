160b622abd0bce60611c4865baf9b064ab7c9550
Yash Gupta <yashguptaofficial@gmail.com>
2019-10-20 15:17:31 +0530

    Added onpopstate event

--- public/router.js
+++ public/router.js
@@ -7,29 +7,13 @@
  *
  */
 
-/*
-Things to accomplish -
-1. Check the URL.
-  - Render the right view if available.
-  - Give a 404 if the view is not available.
-  - Set up backend so that every URL is directed to the 
-  main index file and that file can check for a 404 or route.
-2. Change State on Click
-  - Get the DOM <a> Element/Create the link element 
-  - Change the state and URL on Click
-  
-3. Create new routes.
-  - Views with template tags in HTML
-  - Create or Get Element from the DOM
-*/
-
-// Instantiate an array to store the information of all the routes
+// An Array with objects which contain data about routes
 const routeArray = [];
 
 // Get the main app div
 const app = document.querySelector('#app');
 
-// Defining the structure of our routes
+// Made a Route class
 class Route {
   constructor(name, url, view, query) {
     this.name = name;
@@ -40,34 +24,39 @@ class Route {
   }
 }
 
-// These can be later be assigned using Template Tags in HTML
-// https://www.w3schools.com/tags/tag_template.asp
+// Getting our views from HTML
+// These views are stored with the help of <template> tag
 const homeView = document.querySelector('#home-view').innerHTML;
 const aboutView = document.querySelector('#about-view').innerHTML;
 const contactView = document.querySelector('#contact-view').innerHTML;
 const blogView = document.querySelector('#blog-view').innerHTML;
 
-// Making our new Route objects
+// Instantiating new Route objects
 const home = new Route('home', '/', homeView, 'nav #home');
 const about = new Route('about', '/about', aboutView, 'nav #about');
 const contact = new Route('contact', '/contact', contactView, 'nav #contact');
 const blog = new Route('blog', '/blog', blogView, 'nav #blog');
 
+// Get the current location
 let curLocation = window.location.pathname;
 
-// Check if the current URL matches any decared URLindex
-// Else give a 404
+// Initiate the first route
+history.pushState({ page: "home" }, 'home', '/');
+
 routeArray.every(route => {
+  // Check if the current URL matches the URL of any of our routes
   if (route.url === curLocation) {
+    // Set our view
     app.innerHTML = route.view
     return false;
   } else {
+    // Display a 404
     app.innerHTML = '<h2>Error 404: This page does not exist :/</h2>'
     return true;
   }
 })
 
-// Initialize an onClick function to switch state.
+// Initialize an onClick function to switch our state on Click
 const onRouteClick = (query, view, name, url) => {
   const element = document.querySelector(query);
 
@@ -80,6 +69,21 @@ const onRouteClick = (query, view, name, url) => {
   });
 };
 
+// Called the above function for every route
 routeArray.forEach(route => {
   onRouteClick(route.query, route.view, route.name, route.url);
 });
+
+// Added onpopstate event so that our back and forward buttons work properly
+window.onpopstate = function (e) {
+  routeArray.every(route => {
+    if (route.name === e.state.page) {
+      app.innerHTML = route.view
+      return false;
+    } else {
+      app.innerHTML = '<h2>Error 404: This page does not exist :/</h2>'
+      return true;
+    }
+  })
+};
+
